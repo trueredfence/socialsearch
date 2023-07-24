dd742aafbe857715eb08b66f947e05da551e8b9c
tpsinghcodes <tpsinghcodes@gmail.com>
2023-07-24 21:23:02 +0530

    user name extract update

--- utils/database.js
+++ utils/database.js
@@ -19,7 +19,7 @@ export const connectToDB = async () => {
 
     isConnected = true;
 
-    console.log('MongoDB connected')
+    // console.log('MongoDB connected')
   } catch (error) {
     console.log(error);
   }
