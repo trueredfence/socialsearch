b4f54e777171f4a1083206fff4560fc87c749925
tpsinghcodes <tpsinghcodes@gmail.com>
2023-07-24 21:35:03 +0530

    user name extract update

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <p className='desc text-center'>
       This tool is designed to search social media presence with email and mobile number. We provide 99% accurate data. We don't share or save any query or information provided by you on this platform.
     </p>    
-    <h5 className='support_text text-center'>For Sales & Support  <br/> <span className='orange_gradient text-center'>support@bytesec.in</span></h5>
+    <h5 className='support_text text-center'>For Sales & Support  <br/> <span className='orange_gradient text-center'>support@bytesec.co.in</span></h5>
     <Showresult />
     </section>
   )
