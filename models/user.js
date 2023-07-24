b26f128fdc25a22a0bb37cf58fb98d8c27d1f0f6
tpsinghcodes <tpsinghcodes@gmail.com>
2023-07-24 21:19:31 +0530

    user name extract update

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema({
   },
   username: {
     type: String,    
-    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    match: [/^(?=.{5,30}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
   }, 
   image: {
     type: String,
