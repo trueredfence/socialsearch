741410b4f0617f0b1aaae9aa22dfb9d3203456e2
devtechfusion <devtechfusion@gmail.com>
2023-11-23 22:21:03 +0530

    New API URL Updated

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,43 +1,47 @@
 import { connectToDB } from "@utils/database";
-import  User  from "@models/user"
-import { NextResponse } from 'next/server'
+import User from "@models/user";
+import { NextResponse } from "next/server";
 import { fetchSocialDetails } from "@utils";
 export const POST = async (request) => {
-    const { query, type, user } = await request.json();    
-    let url
-    let userData
-    if(type == "email"){
-      console.log(type);
-      url = "https://api.seon.io/SeonRestService/email-api/v2.2/"+query
+  const { query, type, user } = await request.json();
+  let url;
+  let userData;
+  if (type == "email") {
+    console.log(type);
+    url =
+      "https://api.us-east-1-main.seon.io/SeonRestService/email-api/v2/" +
+      query;
+    // url = "https://api.seon.io/SeonRestService/email-api/v2.2/" + query;
+  }
+  if (type == "mobile") {
+    url =
+      "https://api.us-east-1-main.seon.io/SeonRestService/phone-api/v1/" +
+      query;
+    // url = "https://api.seon.io/SeonRestService/phone-api/v1.4/" + query;
+  }
+
+  try {
+    await connectToDB();
+    const userExists = await User.findById(user);
+    if (userExists) {
+      const apiKey = userExists["api"];
+      const headers = {
+        "X-API-KEY": apiKey,
+        "Content-Type": "application/json",
+      };
+      try {
+        const response = await fetchSocialDetails({
+          url: url,
+          headers: headers,
+        });
+        userData = response;
+      } catch (error) {
+        console.log(error);
+        userData = [];
+      }
     }
-    if(type == "mobile"){
-      url = "https://api.seon.io/SeonRestService/phone-api/v1.4/"+query
-    }
-    
-    try {       
-      await connectToDB();
-      const userExists = await User.findById(user); 
-        if(userExists){
-          const apiKey = userExists['api']
-          const headers = {
-                    'X-API-KEY': apiKey,
-                    'Content-Type':'application/json'
-                }
-          try {     
-            const response = await fetchSocialDetails(
-              {
-                url:url,
-                headers:headers
-              }
-            )          
-            userData = response
-        }catch (error) {
-            console.log(error);
-            userData = []
-        } 
-      }     
-      return new Response(JSON.stringify(userData), { status: 200 })
-    } catch (error) {
-        return new Response(error, { status: 500 });
-    }
-}
+    return new Response(JSON.stringify(userData), { status: 200 });
+  } catch (error) {
+    return new Response(error, { status: 500 });
+  }
+};
--- components/Showresult.jsx
+++ components/Showresult.jsx
@@ -1,38 +1,34 @@
-'use client'
-import { useState, useEffect } from "react"
+"use client";
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import Card from "@components/Card" 
+import Card from "@components/Card";
 
-
-const ResultCardList = ({data}) => { 
-  const accoutDetails=data.data.account_details
-  let activeAccounts = {} 
-  for (let i in accoutDetails){
-    if(accoutDetails[i].registered){      
-      activeAccounts[i] = accoutDetails[i]      
+const ResultCardList = ({ data }) => {
+  console.log(data);
+  const accoutDetails = data.data.account_details;
+  let activeAccounts = {};
+  for (let i in accoutDetails) {
+    if (accoutDetails[i].registered) {
+      activeAccounts[i] = accoutDetails[i];
     }
   }
-  activeAccounts = Object.entries(activeAccounts)  
+  activeAccounts = Object.entries(activeAccounts);
   return (
-    <div className='mt-16 prompt_layout'>
+    <div className="mt-16 prompt_layout">
       {activeAccounts.map((post, index) => (
-      
-      <Card
-        key={index}
-        post={post}
-      />  
+        <Card key={index} post={post} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 const Showresult = () => {
   const router = useRouter();
-  const {data:session} = useSession();
-  const [searchQuery, setsearchQuery] = useState("")
-  const [isSubmitting, setisSubmitting] = useState(false)
-  const [searchedResults, setsearchedResults] = useState([])  
+  const { data: session } = useSession();
+  const [searchQuery, setsearchQuery] = useState("");
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const [searchedResults, setsearchedResults] = useState([]);
 
   const validateEmail = (email) => {
     return email.match(
@@ -40,74 +36,67 @@ const Showresult = () => {
     );
   };
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-    setisSubmitting(true) 
-    let queryType       
-    if(validateEmail(searchQuery)){
-      queryType = "email"          
-    }else{
-      queryType = "mobile"      
-    } 
-    try {      
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setisSubmitting(true);
+    let queryType;
+    if (validateEmail(searchQuery)) {
+      queryType = "email";
+    } else {
+      queryType = "mobile";
+    }
+    try {
       const response = await fetch("/api/search", {
         method: "POST",
         body: JSON.stringify({
           query: searchQuery,
           type: queryType,
-          user: session?.user.id
+          user: session?.user.id,
         }),
       });
-      const data = await response.json();             
-      if (response.ok) { 
-        setsearchedResults(data)  
+      const data = await response.json();
+      if (response.ok) {
+        setsearchedResults(data);
         router.push("/");
       }
     } catch (error) {
       console.log(error);
-      setsearchedResults([])
-    } finally {            
-      setsearchQuery("");      
+      setsearchedResults([]);
+    } finally {
+      setsearchQuery("");
       setisSubmitting(false);
     }
-        
-  }
- 
+  };
+
   return (
     //TODO:check session.users
-    (session &&  
-    <section className='feed'>
-     
-    <form className='relative w-full flex-center' onSubmit={handleSubmit}>
-        <input
-        type='text'
-        placeholder='Search with email & mobile number'
-        onChange={({ target }) =>
-        setsearchQuery(target.value)
-        }
-        value={searchQuery} 
-        //TODO:this need to be verify from old codes
-        required
-        className='search_input'
-        />  
-        <button
-            type='submit'
+    session && (
+      <section className="feed">
+        <form className="relative w-full flex-center" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search with email & mobile number"
+            onChange={({ target }) => setsearchQuery(target.value)}
+            value={searchQuery}
+            //TODO:this need to be verify from old codes
+            required
+            className="search_input"
+          />
+          <button
+            type="submit"
             disabled={isSubmitting}
-            className='px-5 py-1.5 text-sm rounded-r-lg h-10 w=10 bg-primary-orange text-white'
-          > 
-          {isSubmitting ? 'Searching...' : 'Search'}
-          </button>    
-           
-    </form> 
+            className="px-5 py-1.5 text-sm rounded-r-lg h-10 w=10 bg-primary-orange text-white"
+          >
+            {isSubmitting ? "Searching..." : "Search"}
+          </button>
+        </form>
 
-    {searchedResults && Object.keys(searchedResults).length >= 1 && 
-    <ResultCardList
-      data={searchedResults}
-    />  
-    }  
-    </section>
+        {searchedResults && Object.keys(searchedResults).length >= 1 && (
+          <ResultCardList data={searchedResults} />
+        )}
+      </section>
     )
-  )
-}
+  );
+};
 
-export default Showresult
+export default Showresult;
